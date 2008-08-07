80fc5e250beeb3b6862e8d833920ee826db383ff
rasnatty <devnull@localhost>
2008-08-07 11:52:30 +0000

    modified and additional functions and new version number for release 4.0

---
+++ src/classplot.R
@@ -0,0 +1,37 @@
+`classplot` <-
+function(model, xlim, ylim, N = 100,  pch=15, col=NULL, legend=TRUE, position="topright", bg="gray90",  ...)
+{
+if(any(class(model) %in% "svm"))
+{
+priorlabels <- model$levels
+if(ncol(model$SV)!=2)
+stop("data must be two-dimensional")
+}
+else if(any(class(model) %in% c("lda", "qda")))
+{
+priorlabels <- rownames(model$means)
+if(ncol(model$means)!=2)
+stop("data must be two-dimensional")
+}
+        pnts <- cbind(sort(rep(1:N/N, N)), rep(1:N/N, N))
+        pnts[, 1] <- pnts[, 1] * (xlim[2] - xlim[1]) + xlim[1]
+        pnts[, 2] <- pnts[, 2] * (ylim[2] - ylim[1]) + ylim[1]
+if(any(class(model) %in% c("lda", "qda")))
+        blabs <- as.character(predict(model, pnts)$class)
+else if(any(class(model) %in% "svm"))
+	blabs <- as.character(predict(model, pnts))
+        k <- 1
+if(is.null(col))
+        colours <- mu.colour(priorlabels, TRUE, FALSE)$colour
+else
+colours <- col
+plot(pnts, xlim=xlim, ylim=ylim, ...)
+        for (j in priorlabels) {
+            temp <- muclass(blabs, j)
+            points(pnts[temp, ],pch=pch, col = colours[k])
+            k <- k + 1
+        }
+		if(legend)
+		legend(position, legend=priorlabels, col=colours, fill=colours, bg=bg)
+   }
+
--- src/trapply.R
+++ src/trapply.R
@@ -3,45 +3,36 @@ function(trackdata, fun, ..., simplify=FALSE, buildtrack=FALSE)
 {
 
 if(!simplify & !buildtrack)
-	result <- list(NULL)
+result <- list(NULL)
 else
-	result <- NULL
+result <- NULL
 
 
 for(j in 1:nrow(trackdata)){
-   if(!simplify & !buildtrack)
-	   result[[j]] <- fun(trackdata[j,]$data, ...)
-   else
-   {
-      if(!buildtrack)
-	      result <- rbind(result, fun(trackdata[j,]$data, ...))
-      else
-	      result <- c(result, fun(trackdata[j,]$data, ...))
-   }
+if(!simplify & !buildtrack)
+result[[j]] <- fun(trackdata[j,]$data, ...)
+else
+{
+if(!buildtrack)
+result <- rbind(result, fun(trackdata[j,]$data, ...))
+else
+result <- c(result, fun(trackdata[j,]$data, ...))
+}
+}
+
+if(simplify & !buildtrack)
+{
+if (ncol(result) == 1)
+result <- c(result)
 }
 
 if(buildtrack)
 {
-   if (nrow(cbind(result)) != nrow(trackdata$data)) 
-	   warning("Can not turn the result into a trackdata object. Data length differs as a result of FUN.")
-   else {
-	   trackdata$data <- cbind(result)
-	   result <- trackdata
-	   return(result)
-   }
+trackdata$data <- cbind(result)
+result <- trackdata
 }
 
-
-if(simplify)
-{
-   if (ncol(result) == 1) {
-      result <- c(result)
-      return(result)
-   }
-}
-
-
-return(result)
+result
 
 }
 
