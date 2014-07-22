bc5ca9f8c3be09674f2301e61e39780fe146bf2b
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-07-22 12:01:11 +0200

    added stubs for emu.track+emu.query

---
+++ R/emu.query.R
@@ -0,0 +1,12 @@
+##' emu.query
+##' 
+##' stub function to tell user that a new version is beeing implemented
+##' 
+##' 
+##' @keywords internal
+##' @export
+"emu.query" <- function () 
+{
+  stop('emu.query is currenlty not available in this version of the emuR package. We are curerntly working
+          on a new implementation of this function. Check back soon to test the new version...')
+}
