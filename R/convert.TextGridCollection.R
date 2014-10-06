c759768d7a1ed3bceaa0a414e6eb02eee00be691
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-10-06 12:25:00 +0200

    doc + progressbar added/updated

--- R/convert.TextGridCollection.R
+++ R/convert.TextGridCollection.R
@@ -1,16 +1,37 @@
+##' Convert a TextGridCollection (.TextGrid + .wav) to emuDB
 ##' 
-##' 
+##' Converts a TextGridCollection to a emuDB by first generating a file pair list 
+##' containing the paths to .TextGrid + .wav (default extentions) files with the same base
+##' name. It then generates a emuDB DBconfig from the first TextGrid in this file pair list. 
+##' After this it converts all file pairs to the new format.
 ##' 
 ##' @param path2rootDir path to root directory (CAUTION: think of DB size and search space!) 
+##' @param dbName name given to newly generated emuDB
+##' @param targetDir directory where to save newly generated emuDB
 ##' @param tgExt extention of TextGrid files (default=TextGrid meaning filesnames of the form baseName.TextGrid)
 ##' @param audioExt extention of audio files (default=wav meaning filesnames of the form baseName.wav). 
 ##' NOTE: Currently the only supported format accross all components of the new EMU system is the 'wav' audio format .
 ##' 
 ##' 
-convert.TextGridCollection.to.emuDB <- function(path2rootDir, dbName, tgExt='TextGrid', audioExt='wav'){
+convert.TextGridCollection.to.emuDB <- function(path2rootDir, dbName, 
+                                                targetDir, tgExt = 'TextGrid', 
+                                                audioExt = 'wav', showProgress = TRUE){
+  
+  # check if target dir already exists
+  if(file.exists(file.path(targetDir, dbName))){
+    stop('The directory ', file.path(targetDir, dbName), ' already exists. Can not generate new emuDB if directory called ', dbName, ' already exists!')
+  }
   
   # gernerate file pail list
   fpl = create.filePairList(path2rootDir, audioExt, tgExt)
+
+  progress = 0
+  
+  if(showProgress){
+    cat("INFO: Loading TextGridCollection containing", length(fpl[,1]), "bundles...\n")
+    pb = txtProgressBar(min = 0, max = length(fpl[,1]), initial = progress, style=3)
+    setTxtProgressBar(pb, progress)
+  }
   
   # gereate schema from first TextGrid in fpl
   dbd = create.database.schema.from.TextGrid(fpl[1,2], dbName)
@@ -25,7 +46,6 @@ convert.TextGridCollection.to.emuDB <- function(path2rootDir, dbName, tgExt='Tex
   
   # loop through fpl
   for(i in 1:dim(fpl)[1]){
-    #   i = 1
     
     # get sampleRate of audio file
     asspObj = read.AsspDataObj(fpl[i,1])
@@ -55,8 +75,12 @@ convert.TextGridCollection.to.emuDB <- function(path2rootDir, dbName, tgExt='Tex
     
     allBundles[[bndlName]]=bundle
     
+    # update pb
+    if(showProgress){
+      setTxtProgressBar(pb, i)
+    }
+    
   }
-  
   # reassign items as dataframe
   itemsIdx=db[['itemsIdx']]
   db[['items']]=data.frame(id=db[['items']][['id']][1:itemsIdx],bundle=db[['items']][['bundle']][1:itemsIdx],level=db[['items']][['level']][1:itemsIdx],bundleId=db[['items']][['bundleId']][1:itemsIdx],type=db[['items']][['type']][1:itemsIdx],seqIdx=db[['items']][['seqIdx']][1:itemsIdx],sampleRate=db[['items']][['sampleRate']][1:itemsIdx],samplePoint=db[['items']][['samplePoint']][1:itemsIdx],sampleStart=db[['items']][['sampleStart']][1:itemsIdx],sampleDur=db[['items']][['sampleDur']][1:itemsIdx],label=db[['items']][['label']][1:itemsIdx],stringsAsFactors=FALSE)
@@ -73,12 +97,14 @@ convert.TextGridCollection.to.emuDB <- function(path2rootDir, dbName, tgExt='Tex
   containerSession=emuDB.session(name='0000', bundles=allBundles)
   db[['sessions']][['0000']]=containerSession
   
-  return(db)
+  # store newly generated emuDB
+  cat('\n') # hack to have newline after pb
+  store.database(db, targetDir, showProgress = showProgress)
 }
 
 
 
 
 # FOR DEVELOPMENT
-path2rootDir = system.file("extdata/legacy_emu/DBs/", package = "emuR")
-newDB = convert.TextGridCollection.to.emuDB(path2rootDir, 'test12')
+# path2rootDir = system.file("extdata/legacy_emu/DBs/", package = "emuR")
+# convert.TextGridCollection.to.emuDB(path2rootDir, 'test12', '~/Desktop/')
