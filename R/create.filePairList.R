03f98db24e41eda4f7cb14ea5e3db4a89a7608db
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-01-09 12:23:11 +0100

    updated create.filePairList to use ext1 as primary

--- R/create.filePairList.R
+++ R/create.filePairList.R
@@ -1,25 +1,26 @@
 ## Create a file-pair-list
 ##
 ## Recursivly searches through a root directory and matches the
-## basenames of files that have the extentions provided. 
+## basenames of files that have the extentions provided.
 ##
 ## 
-## @param ext1Path2rootDir path to root directory of first file extention (CAUTION: think of DB size and search space!) 
+## @param ext1Path2rootDir path to root directory of first file extention 
 ## @param ext2Path2rootDir path to root directory of second file extention (CAUTION: think of DB size and search space!) 
-## @param ext1 first extention to look for
+## @param ext1 first extention to look for. This extention is considered the primary extention. 
+## This means that this extentions genarates the basename list that the second extentions list is matched against.
 ## @param ext2 second extention to pair base names of first extention with
 ##
 create.filePairList <- function(ext1Path2rootDir, ext2Path2rootDir, ext1, ext2){
+  
   # ext1Path2rootDir is valid path
   if(!file.exists(ext1Path2rootDir)){
     stop('ext1Path2rootDir does not exist!')
   }
-
+  
   # ext2Path2rootDir is valid path
   if(!file.exists(ext2Path2rootDir)){
     stop('ext2Path2rootDir does not exist!')
   }
-  
   
   
   # get all ext1 file paths
@@ -27,27 +28,39 @@ create.filePairList <- function(ext1Path2rootDir, ext2Path2rootDir, ext1, ext2){
   
   # get all audio file paths
   allExt2FilePaths = list.files(ext2Path2rootDir, pattern=paste(ext2, "$", sep = ""), recursive=T, full.names=T)
+
+  # check more ext1 found than ext2
+  if(length(allExt1FilePaths) > length(allExt2FilePaths)){
+    stop('Found less files with ', ext2, ' extention than files with ', ext1, ' extention')
+  }
   
   # extract base names
   allExt1FilePathsBNs = basename(file_path_sans_ext(allExt1FilePaths))
   allExt2FilePathsBNs = basename(file_path_sans_ext(allExt2FilePaths))
   
-  # check for equality
-  if(length(setdiff(allExt1FilePathsBNs, allExt2FilePathsBNs)) != 0){
-    stop('Did not find matching ext1/ext2 file pairs for file base name(s): ', paste(setdiff(allExt1FilePathsBNs, allExt2FilePathsBNs), collapse = ' '))
+  equalToExt1FilePathsBNs = allExt1FilePathsBNs[allExt2FilePathsBNs %in% allExt1FilePathsBNs]
+  foundExt2FilePaths = allExt2FilePaths[allExt2FilePathsBNs %in% allExt1FilePathsBNs]
+  
+  
+  # check if found all allExt2FilePathsBNs in allExt1FilePathsBNs
+  if(length(allExt1FilePathsBNs) != length(equalToExt1FilePathsBNs)){
+    stop('Not all ', ext2, ' files found for ', ext1, ' files found in ', ext1Path2rootDir, ' and ', ext2Path2rootDir ) 
   }
   
-  # check they are the same length (not caught by setdiff())
-  if(length(allExt1FilePathsBNs) != length(allExt2FilePathsBNs)){
-    stop('Not the same amount of ', ext1, ' and ', ext2, ' files found in ', ext1Path2rootDir, ' and ', ext2Path2rootDir ) 
-  }
-
+  
   # check they are empty
   if(length(allExt1FilePathsBNs)==0 || length(allExt2FilePathsBNs) == 0){
     stop('Both colomns in file pair list are empty! That means no files where found...') 
   }
   
   # cbind filePairList
-  fpl = cbind(allExt1FilePaths, allExt2FilePaths)
+  fpl = cbind(allExt1FilePaths, foundExt2FilePaths)
   
+  colnames(fpl) = c('ext1FilePaths', 'ext2FilePaths')
+  
+  return(fpl)
 }
+
+# FOR DEVELOPMENT
+# library('testthat')
+# test_file('tests/testthat/test_create.filePairList.R')
