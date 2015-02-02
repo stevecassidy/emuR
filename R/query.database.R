fc467c8fc377a19d0c1368a61306f4eaccd4ee8e
klausj <klausj@phonetik.uni-muenchen.de>
2015-02-02 16:07:05 +0100

    Improved handling of hash character in linear attribute levels.

--- R/query.database.R
+++ R/query.database.R
@@ -732,7 +732,9 @@ query.database.eql.ETTIKETTA<-function(database,q,labels=NULL){
       if(projectionLevel){
         rIts=res[['items']]
         # projection of the result items themself
-        res[['projectionItems']]=data.frame(seqStartId=rIts[,'seqStartId'],seqEndId=rIts[,'seqEndId'],seqLen=rIts[,'seqLen'],pSeqStartId=rIts[,'seqStartId'],pSeqEndId=rIts[,'seqEndId'],pSeqLen=rIts[,'seqLen'],pLevel=rIts[,'level'],stringsAsFactors = FALSE)
+        lvlNms=rep(lvlName,nrow(rIts))
+        res[['projectionItems']]=data.frame(seqStartId=rIts[,'seqStartId'],seqEndId=rIts[,'seqEndId'],seqLen=rIts[,'seqLen'],pSeqStartId=rIts[,'seqStartId'],pSeqEndId=rIts[,'seqEndId'],pSeqLen=rIts[,'seqLen'],pLevel=lvlNms,stringsAsFactors = FALSE)
+        res[['projectionAttrLevel']]=lvlName
       }
       return(res)
     }
@@ -760,16 +762,21 @@ query.database.eql.KONJA<-function(database,q){
     }
     resultLevel=NULL
     projection=FALSE
+    
+    # parse through all terms of and (&) operation
     while(p>=0){
+      # find ampersand '&' char
       p=get.string.position(string=qTrim,searchStr='&',pos=startPos,literalQuote="'")
       if(p==-1){
+        # get single term
         condStr=str_trim(substring(qTrim,startPos))
       }else{
+        # get leading term
         condStr=str_trim(substr(qTrim,startPos,p-1))
-        
+        # advance to next
         startPos=p+1
       }
-      #cat(condStr,"\n")
+      # execute query on term      
       res=query.database.eql.EA(database,condStr,items=items,labels=labels)
       # set resultLevel of first term
       if(is.null(resultLevel)){
@@ -778,7 +785,6 @@ query.database.eql.KONJA<-function(database,q){
           resultLevel=termResLevel
         }
       }
-      
       
       seqIts=res[['items']]
       nRes=nrow(seqIts)
@@ -790,15 +796,20 @@ query.database.eql.KONJA<-function(database,q){
         items=sqldf("SELECT i.* FROM items i,seqIts s WHERE i.id=s.seqStartId")
         labels=sqldf("SELECT l.* FROM labels l,seqIts s WHERE l.itemID=s.seqStartId")
         if(!is.null(res[['projectionItems']])){
+          # only one term can be marked with hashtag
+          if(projection){
+            stop("Only one hashtag allowed in linear query term: ",qTrim)
+          }
           # if one of the boolean terms is marked with the hashtag the whole term is marked 
           projection=TRUE
+          projectionAttrLevel=res[['projectionAttrLevel']]
         }
       }
     }
     res[['items']][,'level']=resultLevel
     items=res[['items']]
     if(projection){      
-      qStr="SELECT i.seqStartId,i.seqEndId,i.seqStartId AS pSeqStartId ,i.seqEndId AS pSeqEndId,i.seqLen AS pSeqLen,i.level AS pLevel FROM items i"
+      qStr=paste0('SELECT i.seqStartId,i.seqEndId,i.seqStartId AS pSeqStartId ,i.seqEndId AS pSeqEndId,i.seqLen AS pSeqLen,"',projectionAttrLevel,'" AS pLevel FROM items i')
       res[['projectionItems']]=sqldf(qStr)
     }
     res[['resultLevel']]=resultLevel
