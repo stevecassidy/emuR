d88e4b5268c51b6ebd546ef557b4506f5a91cc02
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-02-04 14:28:30 +0100

    added documentation

--- R/autobuild.R
+++ R/autobuild.R
@@ -1,4 +1,21 @@
-autobuild.linkFromTimes <- function(db, superlevelName, sublevelName){
+##' Autobuild links between two levels using their time information
+##' 
+##' Autobuild links between two time levels. The super-level has to be of the 
+##' type SEGMENT and the sub-level either of type EVENT or of type SEGMENT. If
+##' this is the case and a according link definition is present in db$DBconfig$linkDefintions,
+##' this function automatically links the events or segments of the sub-level to
+##' the super-levels segments which contain (time) these.
+##' 
+##' @param db emuDB object to perform autobuild on
+##' @param superlevelName name of level to link from (link definition required in db)
+##' @param sublevelName name of level to link to (link definition required in db)
+##' @param writeToDisc should changes be written to disk completing after autobuild
+##' @return an emuDB object
+##' @author Raphael Winkelmann
+##' @import RSQLite
+##' @keywords emuR autobuild
+##
+autobuild.linkFromTimes <- function(db, superlevelName, sublevelName, writeToDisc = TRUE){
   
   foundSuperLevelDev = NULL
   foundSubLevelDev = NULL
@@ -23,7 +40,7 @@ autobuild.linkFromTimes <- function(db, superlevelName, sublevelName){
     stop('Did not find linkDefintion for: ', superlevelName, ' ', sublevelName, '. Defined linkDefinitions are: ', sapply(db$DBconfig$linkDefinitions, function(x){paste0(x$superlevelName, '->', x$sublevelName, '; ')}))
   }
   
-  
+  # table names
   itemsTableName = "emuR_emuDB_items_tmp"
   
   labelsTableName ="emuR_emuDB_labels_tmp"
@@ -37,15 +54,15 @@ autobuild.linkFromTimes <- function(db, superlevelName, sublevelName){
   dbWriteTable(con, itemsTableName, db$items)
   dbWriteTable(con, labelsTableName, db$labels)
   dbWriteTable(con, linksTableName, db$links)
-
+  
   # query DB depending on type of sublevelDefinition 
   if(foundSubLevelDev$type == 'EVENT'){
     res=dbSendQuery(con, paste0("INSERT INTO ", linksTableName, " (session, bundle, fromID, toID) ",
-                            " SELECT it1.session, it1.bundle, it1.itemID, it2.itemID", 
-                            " FROM ", itemsTableName, " as it1 JOIN ", itemsTableName, " as it2 ",
-                            " WHERE it1.level = '", superlevelName, "'", " AND it2.level = '", sublevelName, "'", 
-                            " AND it1.session = it2.session", " AND it1.bundle = it2.bundle",
-                            " AND it2.samplePoint >= it1.sampleStart", " AND it2.samplePoint <= (it1.sampleStart + it1.sampleDur)")) # only for EVENT sublevel
+                                " SELECT it1.session, it1.bundle, it1.itemID, it2.itemID", 
+                                " FROM ", itemsTableName, " as it1 JOIN ", itemsTableName, " as it2 ",
+                                " WHERE it1.level = '", superlevelName, "'", " AND it2.level = '", sublevelName, "'", 
+                                " AND it1.session = it2.session", " AND it1.bundle = it2.bundle",
+                                " AND it2.samplePoint >= it1.sampleStart", " AND it2.samplePoint <= (it1.sampleStart + it1.sampleDur)")) # only for EVENT sublevel
   }else{
     dbSendQuery(con, paste0("INSERT INTO ", linksTableName, " (session, bundle, fromID, toID) ",
                             "SELECT it1.session, it1.bundle, it1.itemID, it2.itemID", 
@@ -55,22 +72,21 @@ autobuild.linkFromTimes <- function(db, superlevelName, sublevelName){
                             " AND it2.sampleStart >= it1.sampleStart", " AND (it2.sampleStart + it2.sampleDur) <= (it1.sampleStart + it1.sampleDur)")) # only for SEGMENT sublevel    
   }
   
-#     t = dbFetch(res)
-#     dbClearResult(res)
-#     print(t)
+  # extract link dataframe and assign them to db Obj
+  db[['items']]=dbReadTable(con, itemsTableName)
   
-  res = dbSendQuery(con, paste0("SELECT *", 
-                                " FROM ", linksTableName))
-  t = dbFetch(res)
-  dbClearResult(res)
-  print(t)
-  
+  # store changes to disc if desired
+  if(writeToDisc){
+    store.emuDB(db, ae$basePath)
+  }
   
   # Disconnect from the database
   dbDisconnect(con)
+
+  return(db)
   
 }
 
 # FOR DEVELOPMENT 
-library('testthat') 
-test_file('tests/testthat/test_autobuild.R')
+# library('testthat') 
+# test_file('tests/testthat/test_autobuild.R')
