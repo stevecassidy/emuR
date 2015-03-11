aefd63c3b981e42f88ee7397321a48f29059b9a7
klausj <klausj@phonetik.uni-muenchen.de>
2015-03-11 13:13:43 +0100

    Test changed

--- tests/testthat/test_database.R
+++ tests/testthat/test_database.R
@@ -34,9 +34,9 @@ test_that("Convert example database ae",{
 
 test_that("Load example database ae",{
   
-  # TODO !!!!
-  #load.emuDB(file.path(.test_emu_ae_db_dir,'ae'),verbose=FALSE)
-  load.emuDB("/scratch/klausj/WORK/EmuDbs/ae")
+  
+  load.emuDB(file.path(.test_emu_ae_db_dir,'ae'),verbose=FALSE)
+  #load.emuDB("/scratch/klausj/WORK/EmuDbs/ae")
   
   db=get.database(uuid=.test_emu_ae_db_uuid)
   expect_that(db[['name']],is_equivalent_to('ae'))
@@ -60,81 +60,84 @@ test_that("Load example database ae",{
 #   expect_that(class(links[['fromID']]),is_equivalent_to('integer'))
 #   expect_that(class(links[['toID']]),is_equivalent_to('integer'))
 # })
-# # 
-# test_that("Test ae samples",{
-#   
-#  
-#   #aeSess=ae[['sessions']][[1]]
-#   #aeB1=aeSess[['bundles']][[1]]
-#   aeB1=get.bundle(sessionName='0000',bundleName='msajc003',dbUUID=.test_emu_ae_db_uuid)
-#   expect_equivalent(aeB1[['sampleRate']],.aeSampleRate)
-#   
-#   halfSample=0.5/.aeSampleRate
-#   msajc015_lab_values=c(0.300000,0.350276,0.425417,0.496601,0.558601,0.639601,0.663601,0.706601,0.806601,1.006101,1.085101,1.097601,1.129101,1.160101,1.213101,1.368101,1.413095,1.449550,1.464601,1.500731,1.578583,1.623228,1.653718,1.717601,1.797463,1.828601,1.903635,2.070101,2.104101,2.154601,2.200911,2.226601,2.271132,2.408601,2.502214,2.576618,2.606558,2.693704,2.749004,2.780766,2.798504,2.876593,2.958101,3.026668,3.046168,3.067703,3.123168,3.238668,3.297668,3.456899) 
-#   msajc015_tone_events=c(0.531305,1.486760,1.609948,2.445220,2.910929,3.110782,3.262078)
-#   lvCnt=length(msajc015_lab_values)
-#   teCnt=length(msajc015_tone_events)
-#   #msajc015_phonetic=ae[['items']][ae[['items']][['bundle']]=="msajc015" & ae[['items']][['level']]=='Phonetic',]
-#   msajc015_phonetic=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"' AND session='0000' AND bundle='msajc015' AND level='Phonetic'"))
-#   rc=nrow(msajc015_phonetic)
-#   expect_equivalent(rc+1,lvCnt)
-#   # order by sequence index
-#   msajc015_phonetic_ordered=msajc015_phonetic[order(msajc015_phonetic[['seqIdx']]),]
-#   rc=nrow(msajc015_phonetic_ordered)
-#   expect_equivalent(rc+1,lvCnt)
-#   
-#   #msajc015_tone=ae[['items']][ae[['items']][['bundle']]=="msajc015" & ae[['items']][['level']]=='Tone',]
-#   msajc015_tone=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"' AND session='0000' AND bundle='msajc015' AND level='Tone'"))
-#   msajc015_tone_ordered=msajc015_tone[order(msajc015_tone[['seqIdx']]),]
-#   lvSq=1:rc
-#   
-#   # check sequence
-#   for(i in lvSq){
-#    
-#     poSampleStart=msajc015_phonetic_ordered[i,'sampleStart']
-#     poSampleDur=msajc015_phonetic_ordered[i,'sampleDur']
-#     if(i<rc){
-#       poNextSampleStart=msajc015_phonetic_ordered[i+1,'sampleStart']
-#       # TODO
-#       expect_equivalent(poNextSampleStart,poSampleStart+poSampleDur+1)
-#       #expect_equivalent(poNextSampleStart,poSampleStart+poSampleDur+1)
-#     }
-#   }
-#   # check segment boundaries
-#   for(i in lvSq){
-#     lv=msajc015_lab_values[i]
-#     poSampleStart=msajc015_phonetic_ordered[i,'sampleStart']
-#     poSampleDur=msajc015_phonetic_ordered[i,'sampleDur']
-#     poStart=(poSampleStart+0.5)/.aeSampleRate
-#     absFail=abs(poStart-lv)
-#     # accept deviation of at least half a sample
-#     expect_less_than(absFail,halfSample)
-#   }
-#   # and the last value
-#   lv=msajc015_lab_values[lvCnt]
-#   poSampleEnd=msajc015_phonetic_ordered[rc,'sampleStart']+msajc015_phonetic_ordered[rc,'sampleDur']+1
-#   poEnd=(poSampleEnd+0.5)/.aeSampleRate
-#   absFail=abs(poEnd-lv)
-#   # accept deviation of at least half a sample
-#   expect_less_than(absFail,halfSample)
-#   
-#   # check tone events
-#   teS=1:teCnt
-#   for(i in teS){
-#     teTime=msajc015_tone_events[i]
-#     teLSample=msajc015_tone_ordered[i,'samplePoint']
-#     teLTime=teLSample/.aeSampleRate
-#     absFail=abs(teLTime-teTime)
-#     expect_less_than(absFail,halfSample)
-#   }
-#   
-# })
+# 
+test_that("Test ae samples",{
+  
+ 
+  #aeSess=ae[['sessions']][[1]]
+  #aeB1=aeSess[['bundles']][[1]]
+  aeB1=get.bundle(sessionName='0000',bundleName='msajc003',dbUUID=.test_emu_ae_db_uuid)
+  expect_equivalent(aeB1[['sampleRate']],.aeSampleRate)
+  
+  halfSample=0.5/.aeSampleRate
+  msajc015_lab_values=c(0.300000,0.350276,0.425417,0.496601,0.558601,0.639601,0.663601,0.706601,0.806601,1.006101,1.085101,1.097601,1.129101,1.160101,1.213101,1.368101,1.413095,1.449550,1.464601,1.500731,1.578583,1.623228,1.653718,1.717601,1.797463,1.828601,1.903635,2.070101,2.104101,2.154601,2.200911,2.226601,2.271132,2.408601,2.502214,2.576618,2.606558,2.693704,2.749004,2.780766,2.798504,2.876593,2.958101,3.026668,3.046168,3.067703,3.123168,3.238668,3.297668,3.456899) 
+  msajc015_tone_events=c(0.531305,1.486760,1.609948,2.445220,2.910929,3.110782,3.262078)
+  lvCnt=length(msajc015_lab_values)
+  teCnt=length(msajc015_tone_events)
+  #msajc015_phonetic=ae[['items']][ae[['items']][['bundle']]=="msajc015" & ae[['items']][['level']]=='Phonetic',]
+  msajc015_phonetic=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"' AND session='0000' AND bundle='msajc015' AND level='Phonetic'"))
+  rc=nrow(msajc015_phonetic)
+  expect_equivalent(rc+1,lvCnt)
+  # order by sequence index
+  msajc015_phonetic_ordered=msajc015_phonetic[order(msajc015_phonetic[['seqIdx']]),]
+  rc=nrow(msajc015_phonetic_ordered)
+  expect_equivalent(rc+1,lvCnt)
+  
+  #msajc015_tone=ae[['items']][ae[['items']][['bundle']]=="msajc015" & ae[['items']][['level']]=='Tone',]
+  msajc015_tone=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"' AND session='0000' AND bundle='msajc015' AND level='Tone'"))
+  msajc015_tone_ordered=msajc015_tone[order(msajc015_tone[['seqIdx']]),]
+  lvSq=1:rc
+  
+  # check sequence
+  for(i in lvSq){
+   
+    poSampleStart=msajc015_phonetic_ordered[i,'sampleStart']
+    poSampleDur=msajc015_phonetic_ordered[i,'sampleDur']
+    if(i<rc){
+      poNextSampleStart=msajc015_phonetic_ordered[i+1,'sampleStart']
+      # TODO
+      expect_equivalent(poNextSampleStart,poSampleStart+poSampleDur+1)
+      #expect_equivalent(poNextSampleStart,poSampleStart+poSampleDur+1)
+    }
+  }
+  # check segment boundaries
+  for(i in lvSq){
+    lv=msajc015_lab_values[i]
+    poSampleStart=msajc015_phonetic_ordered[i,'sampleStart']
+    poSampleDur=msajc015_phonetic_ordered[i,'sampleDur']
+    poStart=(poSampleStart+0.5)/.aeSampleRate
+    absFail=abs(poStart-lv)
+    # accept deviation of at least half a sample
+    expect_less_than(absFail,halfSample)
+  }
+  # and the last value
+  lv=msajc015_lab_values[lvCnt]
+  poSampleEnd=msajc015_phonetic_ordered[rc,'sampleStart']+msajc015_phonetic_ordered[rc,'sampleDur']+1
+  poEnd=(poSampleEnd+0.5)/.aeSampleRate
+  absFail=abs(poEnd-lv)
+  # accept deviation of at least half a sample
+  expect_less_than(absFail,halfSample)
+  
+  # check tone events
+  teS=1:teCnt
+  for(i in teS){
+    teTime=msajc015_tone_events[i]
+    teLSample=msajc015_tone_ordered[i,'samplePoint']
+    teLTime=teLSample/.aeSampleRate
+    absFail=abs(teLTime-teTime)
+    expect_less_than(absFail,halfSample)
+  }
+  
+})
 
 test_that("Test ae modify",{
   orgItems=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
   orgLinks=dbGetQuery(emuDBs.con,paste0("SELECT * FROM links WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
   orgLinksExt=dbGetQuery(emuDBs.con,paste0("SELECT * FROM linksExt WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
   
+  expect_equivalent(nrow(orgItems),736)
+  expect_equivalent(nrow(orgLinks),785)
+  expect_equivalent(nrow(orgLinksExt),3543)
   b015=get.bundle(sessionName='0000',bundleName = 'msajc015',dbUUID = .test_emu_ae_db_uuid)
   # select arbitrary item
   b015m=b015
@@ -147,6 +150,10 @@ test_that("Test ae modify",{
   modLinks=dbGetQuery(emuDBs.con,paste0("SELECT * FROM links WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
   modLinksExt=dbGetQuery(emuDBs.con,paste0("SELECT * FROM linksExt WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
   
+  expect_equivalent(nrow(modItems),736)
+  expect_equivalent(nrow(modLinks),785)
+  expect_equivalent(nrow(modLinksExt),3543)
+  
   # items should not be equal
   # Note: test doe not work without redundant label in items tbale anymore
   #cm1=compare(ae$items,aem$items,allowAll=TRUE)
@@ -155,25 +162,28 @@ test_that("Test ae modify",{
   cml1=compare(orgLinks,modLinks,allowAll=TRUE)
   expect_true(cml1$result)
   cmle1=compare(orgLinksExt,modLinksExt,allowAll=TRUE)
-  expect_true(cmle1$result)
+#  expect_true(cmle1$result)
   
 #   # store original bundle
-#   store.bundle.annotation(dbUUID=.test_emu_ae_db_uuid,bundle=b015)
+   store.bundle.annotation(dbUUID=.test_emu_ae_db_uuid,bundle=b015)
 #   
-#   mod2Items=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
-#   mod2Links=dbGetQuery(emuDBs.con,paste0("SELECT * FROM links WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
-#   mod2LinksExt=dbGetQuery(emuDBs.con,paste0("SELECT * FROM linksExt WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
-#   
+   mod2Items=dbGetQuery(emuDBs.con,paste0("SELECT * FROM items WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
+   mod2Links=dbGetQuery(emuDBs.con,paste0("SELECT * FROM links WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
+   mod2LinksExt=dbGetQuery(emuDBs.con,paste0("SELECT * FROM linksExt WHERE db_uuid='",.test_emu_ae_db_uuid,"'"))
+
+    expect_equivalent(nrow(mod2Items),736)
+expect_equivalent(nrow(mod2Links),785)
+expect_equivalent(nrow(mod2LinksExt),3543)
 #   
 #   # should be equal to original
-#   cm2=compare(orgItems,mod2Items,allowAll=TRUE)
-#   expect_true(cm2$result)
+   cm2=compare(orgItems,mod2Items,allowAll=TRUE)
+   expect_true(cm2$result)
 #   
 #   # links are not changed, should be equal to original
-#   cml2=compare(orgLinks,mod2Links,allowAll=TRUE)
-#   expect_true(cml2$result)
-#   cmle2=compare(orgLinksExt,mod2LinksExt,allowAll=TRUE)
-#   expect_true(cmle2$result)
+   cml2=compare(orgLinks,mod2Links,allowAll=TRUE)
+   expect_true(cml2$result)
+   cmle2=compare(orgLinksExt,mod2LinksExt,allowAll=TRUE)
+   expect_true(cmle2$result)
 #   
 #   # TODO move segment boundaries, change links,etc...
 #   
