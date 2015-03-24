a3d80543279cbaf75d24fe5113303a74a25a5c33
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-03-24 15:27:36 +0100

    fixed old variable name in comment

--- R/create.filePairList.R
+++ R/create.filePairList.R
@@ -31,7 +31,7 @@ create.filePairList <- function(ext1Path2rootDir, ext2Path2rootDir, ext1, ext2){
   # get all ext1 file paths
   allExt1FilePaths = list.files(ext1Path2rootDir, pattern=paste(ext1, "$", sep = ""), recursive=T, full.names=T)
   
-  # get all audio file paths
+  # get all ext2 file paths
   allExt2FilePaths = list.files(ext2Path2rootDir, pattern=paste(ext2, "$", sep = ""), recursive=T, full.names=T)
 
   # check more ext1 found than ext2
