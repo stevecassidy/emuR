afcb3aae044dee9e6b8cd14a001d29e0ab85ebb7
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-03-24 17:09:40 +0100

    add.files.to.bundles function added

--- tests/testthat/test_add.files.to.emuDB.R
+++ tests/testthat/test_add.files.to.emuDB.R
@@ -5,7 +5,7 @@ context("testing add.files.to.emuDB functions")
 
 path2ae = system.file("extdata/emu/DBs/ae/", package = "emuR")
 
-path2legacy_ae = system.file("extdata/legacy_emu/DBs/ae/", package = "emuR")
+path2tgs = system.file("extdata/legacy_emu/DBs/ae/labels", package = "emuR")
 
 # copy db to tempdir
 file.copy(path2ae, tempdir(), recursive = T)
@@ -15,12 +15,11 @@ path2newDB = file.path(tempdir(),'ae')
 
 ##############################
 test_that("files are copied and added correctly", {
-  ae = load.emuDB(path2newDB, verbose = F)
-  add.files.to.emuDB(emuDB = ae, path2rootDir = path2legacy_ae, fileExt = 'fms')
-  fmsFilePaths = list.files(path2newDB, pattern = '*.fms', recursive = T)
+  add.files.to.bundles(path2rootDir = path2tgs, path2sessionDir = file.path(path2newDB, '0000_ses'), fileExt = 'TextGrid')
+  tgFilePaths = list.files(path2newDB, pattern = '*TextGrid$', recursive = T)
   
-  expect_equal(length(fmsFilePaths), 7)
-  expect_equal(fmsFilePaths[1], file.path('0000_ses', 'msajc003_bndl', 'msajc003.fms'))
+  expect_equal(length(tgFilePaths), 7)
+  expect_equal(tgFilePaths[1], file.path('0000_ses', 'msajc003_bndl', 'msajc003.TextGrid'))
   
 })
 
