fb6d10029586a53ddd8726553cd7a7f09952f3c6
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-06-09 10:30:14 +0200

    more caching stuff

--- R/database.caching.R
+++ R/database.caching.R
@@ -1,11 +1,14 @@
 ##' Update cache of emuDB
 ##' 
+##' Updates sqlite cache of loaded emuDB. This can and should be used
+##' to update changes to precached/loaded DBs as it only updates the deltas 
+##' in the cache which is considerably faster than reloading and therefore 
+##' recacheing the entire DB.
 ##' @param dbName name of EMU database
 ##' @param dbUUID optional UUID of emuDB
 ##' @author Raphael Winkelmann
 ##' @export
 ##' @keywords emuDB database Emu 
-##' @examples
 update_cache <- function(dbName, dbUUID=NULL){
   #########################
   # get dbObj
@@ -29,27 +32,94 @@ update_cache <- function(dbName, dbUUID=NULL){
   if(!file.exists(dbCfgPath)){
     stop("Could not find database info file: ",dbCfgPath,"\n")
   }
-  # calc. md5 sum
-  new.MD5DBconfigJSON = md5sum(normalizePath(dbCfgPath))
+  # calc. new md5 sum
+  new.MD5DBconfigJSON = md5sum(normalizePath(dbCfgPath))[[1]]
   
-  old.MD5DBconfigJSON = dbGetQuery(getEmuDBcon(), paste0("SELECT MD5DBconfigJSON FROM emuDB WHERE uuid='", dbUUID, "'"))$MD5DBconfigJSON
-  if(old.MD5DBconfigJSON != new.MD5DBconfigJSON){
+  old.MD5DBconfigJSON = dbGetQuery(getEmuDBcon(), paste0("SELECT MD5DBconfigJSON FROM emuDB WHERE uuid='", dbUUID, "'"))[[1]]
+  if(is.na(old.MD5DBconfigJSON) | old.MD5DBconfigJSON != new.MD5DBconfigJSON){
     print('Reloading _DBconfig.json ...')
     # load DBconfig
     schema=load.emuDB.DBconfig(dbCfgPath)
     # set transient values
     schema=.update.transient.schema.values(schema)
     # create db object
-    db=create.database(name = schema[['name']],basePath = normalizePath(databaseDir),DBconfig = schema)
-    # store
-    .store.emuDB.DBI(db, MD5DBconfigJSON)
+    db=create.database(name = schema[['name']],basePath = normalizePath(dbObj$basePath),DBconfig = schema)
+    
+    dbCfgJSON=jsonlite::toJSON(db$DBconfig, auto_unbox=TRUE, force=TRUE, pretty=TRUE)
+    # update entry
+    dbGetQuery(getEmuDBcon(), paste0("UPDATE emuDB SET DBconfigJSON = '", dbCfgJSON , "', ",
+                                     "MD5DBconfigJSON = '", new.MD5DBconfigJSON, "' ",
+                                     "WHERE uuid = '", dbUUID, "'"))
+    
   }
   
   ######################################
   # check which _annot.json files need reloading
-  bt = dbReadTable(getEmuDBcon(), "bundle")
-  print(bt)
   
+  # todo remove old entries in case of rename
+  
+  bt = dbReadTable(getEmuDBcon(), "bundle")
+  bndls = list_bundles(dbName = dbName, dbUUID = dbUUID)
+  
+  sesPattern=paste0('.*', session.suffix, '$')
+  sesPaths=list.files(path=dbObj$basePath, sesPattern)
+  
+  for(s in sesPaths){
+    bndlPattern=paste0('.*', bundle.dir.suffix, '$')
+    bndlPaths=list.files(path=file.path(dbObj$basePath, s), bndlPattern)
+    for(b in bndlPaths){
+      bn = gsub(bundle.dir.suffix,"", b)
+      # calc. new md5 sum
+      annotPath = file.path(dbObj$basePath, s, b, paste0(bn, bundle.annotation.suffix, ".json"))
+      new.MD5annotJSON = md5sum(normalizePath(annotPath))
+      
+      old.MD5annotJSON = dbGetQuery(getEmuDBcon(), paste0("SELECT MD5annotJSON FROM bundle WHERE ",
+                                                          "db_uuid='", dbUUID, "' AND ",
+                                                          "session='", gsub(session.suffix,"", s), "' AND ",
+                                                          "name='", bn, "'"))$MD5annotJSON
+      
+      # set to empty string if NA or empty character()
+      if(length(old.MD5annotJSON) == 0){
+        old.MD5annotJSON = ""
+      }
+      if(is.na(old.MD5annotJSON)){
+        old.MD5annotJSON = ""
+      }
+      
+      
+      if(old.MD5annotJSON != new.MD5annotJSON){
+        print('Reloading _annot.json ...')
+        
+        annoJSONLns=readLines(annotPath,encoding="UTF-8")
+        annoJSON=paste(annoJSONLns,collapse='')
+        bundle=jsonlite::fromJSON(annoJSON,simplifyVector=FALSE)
+        #class(bundle) <- 'emuDB.bundle'
+        bundle=as.bundle(bundle)
+        # set missing fields
+        namedLevels=set.list.names(bundle[['levels']],'name')
+        bundle[['levels']]=namedLevels
+        bundle[['mediaFilePath']]=file.path(file.path(dbObj$basePath, s, b),bundle[['annotates']])
+        bundle[['db_UUID']]=dbObj$DBconfig[['UUID']]
+        # set session name
+        bundle[['session']]=gsub(session.suffix,"", s)
+        
+        
+        # update MD5 value of DBI model in bundle table
+        dbGetQuery(getEmuDBcon(), paste0("UPDATE bundle SET MD5annotJSON = '", new.MD5annotJSON , "' ",
+                                         "WHERE db_uuid = '", dbUUID, "' AND ",
+                                         "      session = '", gsub(session.suffix,"", s), "' AND ",
+                                         "      name = '", bn, "'"))
+        
+        # delete old items/label/links entries
+        .remove.bundle.annot.DBI(dbUUID=dbUUID,bundle=bundle)
+        # and store them
+        .store.bundle.annot.DBI(dbUUID=dbUUID,bundle=bundle)
+        
+      }
+      
+    }
+    
+  }
   
 }
 
--- tests/testthat/test_database.caching.R
+++ tests/testthat/test_database.caching.R
@@ -14,6 +14,70 @@ if(!is.emuDB.loaded("ae")){
   load_emuDB(path2ae, verbose = F)
 }
 
-d = list_emuDBs()
+############################
+test_that("update_cache works", {
+  # pre clean (just in case)
+  unlink(file.path(tempdir(),tmpDbName), recursive = TRUE)
+  
+  # make copy of ae to mess with
+  fp = file.path(tempdir(), tmpDbName)
+  duplicate.loaded.emuDB("ae", tmpDbName, fp)
+  
+  dbUUID = get_emuDB_UUID(dbName = tmpDbName, dbUUID = NULL)
+  dbObj = .load.emuDB.DBI(uuid = dbUUID)
+  
+  # actually store DB to fs
+  store("ae", tempdir(), showProgress=F)
+  file.rename(file.path(tempdir(), "ae"), file.path(tempdir(), tmpDbName))
+  file.rename(file.path(tempdir(), tmpDbName, "ae_DBconfig.json"), file.path(tempdir(), tmpDbName, "ae_copy_DBconfig.json"))
+  
+  ################################
+  # DBconfig update is re-cached
+  
+  # change entry
+  dbJson = fromJSON(readLines(file.path(tempdir(), tmpDbName, "ae_copy_DBconfig.json")), simplifyVector=T)
+  
+  dbJson$name = "ae_copy"
+  
+  pbpJSON=jsonlite::toJSON(dbJson,auto_unbox=TRUE,force=TRUE,pretty=TRUE)
+  writeLines(pbpJSON,file.path(tempdir(), tmpDbName, "ae_copy_DBconfig.json"))
+  
+  update_cache(tmpDbName)
+  
+  dbObj = .load.emuDB.DBI(uuid = dbUUID)
+  
+  expect_equal(dbObj$name, "ae_copy")
+  
+  ################################
+  # new bundle in new session is re-cached
+  dir.create(file.path(tempdir(),'ae_copy', 'new_ses'))
+  file.copy(from = file.path(tempdir(),'ae_copy', '0000_ses', 'msajc012_bndl'), 
+            to = file.path(tempdir(),'ae_copy', 'new_ses'),
+            recursive = T)
+  
+  update_cache(tmpDbName)
+  
+  l = list_sessions(tmpDbName)
+  
+  print(l)
+  
+  ################################
+  # deleted bundle is re-cached
+  # unlink(file.path(tempdir(),'ae_copy', '0000_ses', 'msajc012_bndl'), recursive = TRUE)
+  
+  # update_cache(tmpDbName)
+  
+    
+  # cleanup 
+  unlink(file.path(tempdir(),'ae_copy'), recursive = TRUE)
+  # clean up
+  if(is.emuDB.loaded(tmpDbName)){
+    UUID = get_emuDB_UUID(dbName = tmpDbName)
+    .purge.emuDB(UUID)
+  }
+  
+  
+})
 
-update_cache('ae')
+
+
