4f621ca9cb0b595d345e3d5fd2c9ebc2aef209a6
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-06-17 13:42:21 +0200

    test_validate using new data

--- tests/testthat/test_validate.R
+++ tests/testthat/test_validate.R
@@ -3,7 +3,9 @@
 # ##' @author Raphael Winkelmann
 context("testing validate.XXX.bundle functions")
 
-path2tg = system.file("extdata/legacy_emu/DBs/ae/labels/msajc003.TextGrid", package = "emuR")
+path2demoData = file.path(tempdir(),"emuR_demoData")
+
+path2tg = file.path(path2demoData, "TextGrid_collection/msajc003.TextGrid")
 
 newDbName = "ae_copy"
 
