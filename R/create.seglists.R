52231dcaf934f3f7e8ba2938ed3dca2465583090
klausj <klausj@phonetik.uni-muenchen.de>
2015-06-18 15:54:22 +0200

    Added new segment list conversion function which is able to handle input segment lists with different sequnec length per row.

--- R/create.seglists.R
+++ R/create.seglists.R
@@ -437,6 +437,241 @@ convert.query.result.to.segmentlist<-function(dbConfig,result,timeRefSegmentLeve
   return(segmentList)
 }
 
+
+# convert to emuRsegs segemnt list, variable sequenec length of input allowed
+convert.query.result.to.segmentlist.var<-function(dbConfig,result,timeRefSegmentLevel=NULL){
+  its=NULL
+  
+  items=getQueryTmpEmuDBs()[['queryItems']]
+  
+  bundles=c()
+  labels=c()
+  start=c()
+  end=c()
+  slType=NULL
+  
+  projectionItems=result[['projectionItems']]
+  if(!is.null(projectionItems)){ 
+    # projection result (hashtag marker)
+    its=data.frame(db_uuid=projectionItems[,'db_uuid'],session=projectionItems[,'session'],bundle=projectionItems[,'bundle'],seqStartId=projectionItems[,'pSeqStartId'],seqEndId=projectionItems[,'pSeqEndId'],seqLen=projectionItems[,'pSeqLen'],level=projectionItems[,'pLevel'],stringsAsFactors = FALSE)
+  }else{
+    # use "normal" result
+    its=result[['items']]
+  }
+  
+  # get distinct result levels ...
+  distinctLevels=sqldf("SELECT DISTINCT level FROM its")
+  for(attrNm in distinctLevels[,'level']){
+    
+    lvlNm=get.level.name.for.attribute(dbConfig = dbConfig,attributeName = attrNm)
+    ld=get.levelDefinition(DBconfig = dbConfig,name = lvlNm)
+    #cat("Level ",ld['name']," type ",ld['type'],"\n")
+    if(ld['type']=='ITEM'){
+      segLvlNms=find.segment.levels(dbConfig,attrNm)
+      if(!is.null(timeRefSegmentLevel)){
+        if(!(timeRefSegmentLevel %in% segLvlNms)){
+          stop("Cannot resolve time information for result level '",attrNm,"' using segment time reference level '",timeRefSegmentLevel,"'\nPlease set one of these levels for timeRefSegmentLevel parameter: ",paste(segLvlNms,collapse=', '),".")
+        }
+      }else{
+        segLvlsCnt=length(segLvlNms)
+        if(segLvlsCnt>1){
+          
+          stop("Segment time information derivation for level '",attrNm,"' is ambiguous:\nThe level is linked to multiple segment levels: ",paste(segLvlNms,collapse=', '),"\nPlease select one of these levels using the 'timeRefSegmentLevel' query parameter.")
+        }
+      }
+    }
+    
+  }
+  
+  
+  itCount=nrow(its)
+  if(itCount==0){
+    its=data.frame(db_uuid=character(0),session=character(0),bundle=character(0),seqStartId=integer(0),seqEndId=integer(0),seqLen=integer(0),level=character(0),stringsAsFactors = FALSE)
+  }
+  links=getQueryTmpEmuDBs()[['queryLinksExt']]
+  
+  lblsDf=getQueryTmpEmuDBs()[['queryLabels']]
+  itemsIdxSql='CREATE INDEX items_idx ON items(itemID,db_uuid,session,bundle,level,itemID,seqIdx,type,sampleRate,sampleStart,sampleDur,samplePoint)'
+  resIdxSql='CREATE INDEX its_idx ON its(db_uuid,session,bundle,seqStartId,seqEndId,seqLen,level)'
+  
+  #labelsIdxSql='CREATE INDEX labels_idx ON lblsDf(itemID,name)'
+  labelsIdxSql='CREATE INDEX labels_idx ON lblsDf(itemID,db_uuid,session,bundle,name)'
+  # get max length
+  #itemsIdxSql='CREATE INDEX items_idx ON items(seqLen)'
+  if(itCount>0){
+    maxSeqLenDf=sqldf(c(resIdxSql,"SELECT max(seqLen) AS maxSeqLen FROM its"))
+    maxSeqLen=maxSeqLenDf[1,'maxSeqLen']
+    
+    # for string conacatenation: we need all occuring seq lengths 
+    # distinct sequence lengths
+    distinctSeqLens=sqldf(c(resIdxSql,"SELECT DISTINCT seqLen FROM its"))
+  }else{
+    maxSeqLen=1L
+  }
+  
+  
+  # query seglist data except labels
+  # for this data the information in start end item of the sequence is sufficient
+  # it takes only the start  and end items of the query result in account
+  # the CASE WHEN THEN ELSE END terms are necessary to get the start and end samples of sequences which are not segment levels and therefore have no time information  
+  hasLinks=(nrow(links)>0)
+  
+  
+  # check for ambigious time information (multiple SEGMENT levels)
+  # TODO   
+  #if(hasLinks){
+  #  # items of type ITEM have no (sample) time information
+  #  # therefore we search for linked SEGMENT items and take their start sample position
+  #  qStr=paste0("SELECT count(l.fromID), FROM links l,items s,items e,its r WHERE s.db_uuid=s.db_uuid AND s.session=l.session AND s.bundle=l.bundle AND s.itemID=l.fromID  AND l.toSeqIdx=0 AND e.db_uuid=s.db_uuid AND e.session=s.session AND e.bundle=s.bundle AND r.db_uuid=s.db_uuid AND r.session=s.session AND r.bundle=s.bundle AND s.itemID=r.seqStartId AND e.itemID=r.seqEndId AND e.level=s.level")
+  #  print(sqldf(qStr))
+  #}
+  
+  # select columns: id,session,bundle,startItemId,endItemID ,type ...
+  selectStr="SELECT s.db_uuid ,s.session,s.bundle,s.itemID AS startItemID ,e.itemID AS endItemID,s.type, "
+  
+  # find sequence start position
+  # use sample start of sequence start item for type SEGMENT and samplePoint for type EVENT 
+  selectStr=paste0(selectStr,"CASE s.type \
+                   WHEN 'SEGMENT' THEN s.sampleStart \
+                   WHEN 'EVENT' THEN s.samplePoint ")
+  # calculate start sample for type ITEM
+  if(hasLinks){
+    # items of type ITEM have no (sample) time information
+    # therefore we search for linked SEGMENT items and take their start sample position
+    selectStr=paste0(selectStr," ELSE (SELECT i.sampleStart FROM items i WHERE i.db_uuid=s.db_uuid AND i.session=s.session AND i.bundle=s.bundle AND i.type='SEGMENT' AND ")
+    if(!is.null(timeRefSegmentLevel)){
+      selectStr=paste0(selectStr," i.level='",timeRefSegmentLevel,"' AND ")
+    }
+    selectStr=paste0(selectStr," EXISTS (SELECT * FROM links l WHERE s.db_uuid=s.db_uuid AND s.session=l.session AND s.bundle=l.bundle AND s.itemID=l.fromID AND i.db_uuid=l.db_uuid AND i.session=l.session AND i.bundle=l.bundle AND i.itemID=l.toID AND l.toSeqIdx=0)) ")
+  }else{
+    # TODO
+    # No sample start information. (throw error ?)
+  }
+  selectStr=paste0(selectStr," END AS sampleStart, ")
+  
+  # find sequence end position
+  # use sample start plus sample duration of sequence end item for type SEGMENT and zero for type EVENT
+  # TODO is zero correct here ?? should it be e.samplePoint instead ??
+  selectStr=paste0(selectStr," CASE s.type \
+                   WHEN 'SEGMENT' THEN (e.sampleStart+e.sampleDur) \
+                   WHEN 'EVENT' THEN NULL ")  
+  if(hasLinks){
+    # items of type ITEM have no (sample) time information
+    # therefore we search for linked SEGMENT items and take their end sample position
+    
+    selectStr=paste0(selectStr," ELSE (SELECT i.sampleStart+i.sampleDur FROM items i WHERE i.db_uuid=e.db_uuid AND i.session=e.session AND i.bundle=e.bundle AND i.type='SEGMENT' AND ")
+    if(!is.null(timeRefSegmentLevel)){
+      selectStr=paste0(selectStr," i.level='",timeRefSegmentLevel,"' AND ")
+    }
+    selectStr=paste0(selectStr," EXISTS (SELECT * FROM links l WHERE e.db_uuid=l.db_uuid AND e.session=l.session AND e.bundle=l.bundle AND e.itemID=l.fromID AND i.db_uuid=l.db_uuid AND i.session=l.session AND i.bundle=l.bundle AND i.itemID=l.toID AND l.toSeqIdx+1=l.toSeqLen)) ")
+    
+  }
+  selectStr=paste0(selectStr,"END AS sampleEnd, ")
+  
+  # find samplerate
+  # use sample rate of sequence start item for type SEGMENT and EVENT 
+  selectStr=paste0(selectStr,"CASE s.type \
+                   WHEN 'SEGMENT' THEN s.sampleRate \
+                   WHEN 'EVENT' THEN s.sampleRate ")
+  if(hasLinks){
+    # items of type ITEM have no sample rate information
+    # therefore we search for linked SEGMENT items and take their start sample position
+    # TODO Can we use EVENT items as well ?
+    selectStr=paste0(selectStr," ELSE (SELECT i.sampleRate FROM items i WHERE i.db_uuid=s.db_uuid AND i.session=s.session AND i.bundle=s.bundle AND i.type='SEGMENT' AND ")
+    if(!is.null(timeRefSegmentLevel)){
+      selectStr=paste0(selectStr," i.level='",timeRefSegmentLevel,"' AND ")
+    }
+    selectStr=paste0(selectStr," EXISTS (SELECT * FROM links l WHERE s.itemID=l.fromID AND i.itemID=l.toID AND i.db_uuid=l.db_uuid AND i.session=l.session AND i.bundle=l.bundle AND l.toSeqIdx=0)) ")
+    
+  }else{
+    # TODO no samplerate , error ??
+  }
+  
+  selectStr=paste0(selectStr," END AS sampleRate, ")
+  
+  # from clause
+  fromStr="FROM items s,items e,its r "
+  
+  # where clause: make sure start and end are in same emuDB, session and bundle, select start and end id
+  whereStr="WHERE e.db_uuid=s.db_uuid AND e.session=s.session AND e.bundle=s.bundle AND r.db_uuid=s.db_uuid AND r.session=s.session AND r.bundle=s.bundle AND s.itemID=r.seqStartId AND e.itemID=r.seqEndId AND e.level=s.level "
+  
+  # order
+  orderStr=''
+  
+  selectStr=paste0(selectStr," CASE r.seqLen ")
+  distinctSeqLensLength=nrow(distinctSeqLens)
+  # for each seq len, which occurs in the input segemnt list
+  for(si in 1:distinctSeqLensLength){
+    seqLen=distinctSeqLens[si,1]
+  # append terms depending on maximum sequence length
+  # build query for label sequence string
+  # emuR hierachical requery produces results with differnet seq lengths per row
+    selectStr=paste0(selectStr,"WHEN ",seqLen," THEN ")
+  for(seqIdx in 1:seqLen){
+     selectStr=paste0(selectStr,'(SELECT l.label FROM lblsDf l,items i WHERE l.db_uuid=i.db_uuid AND l.session=i.session AND l.bundle=i.bundle AND l.itemID=i.itemID AND l.name=r.level AND ')
+
+     offset=seqIdx-1
+     selectStr=paste0(selectStr,'i.db_uuid=s.db_uuid AND i.session=s.session AND i.bundle=s.bundle AND i.level=s.level AND i.seqIdx=s.seqIdx+',offset,")")
+     if(seqIdx<seqLen){
+       selectStr=paste0(selectStr," || '->' || ")
+     }
+     
+  }
+    
+  }
+  selectStr=paste0(selectStr," END AS labels ")
+
+  queryStr=paste(selectStr,fromStr,whereStr,orderStr,sep = ' ')
+  #cat(queryStr,"\n")
+  
+  # build indices
+  #itemsIdxSql='CREATE INDEX items_idx ON items(id,session,bundle,level,itemID,seqIdx,type)' # very slow !!
+  itemsIdxSql='CREATE INDEX items_idx ON items(type,db_uuid,session,bundle,level,itemID,seqIdx)'
+  linksIdxSql='CREATE INDEX links_idx ON links(db_uuid,session,bundle,fromID,toID,toSeqIdx,toSeqLen)'
+  if(hasLinks){
+    #st=system.time((segListData=sqldf(c(itemsIdxSql,linksIdxSql,resIdxSql,q))))
+    #cat(st," (with links)\n")
+    segListData=sqldf(c(itemsIdxSql,linksIdxSql,resIdxSql,labelsIdxSql,queryStr))
+  }else{
+    #st=system.time((segListData=sqldf(c(itemsIdxSql,resIdxSql,q))))
+    #cat(st," (without links)\n")
+    segListData=sqldf(c(itemsIdxSql,resIdxSql,labelsIdxSql,queryStr))
+  }
+  # Note: CASE s.type WHEN 'SEGMENT' OR 'EVENT' did not work.
+  
+  # convert samples to milliseconds using SQL:
+  seglist=sqldf("SELECT \
+                       labels,
+                       CASE type WHEN 'EVENT' THEN \
+                        CAST (sampleStart AS REAL)/ CAST( sampleRate AS REAL) * 1000.0 \
+                       ELSE \
+                        (CAST (sampleStart AS REAL) + 0.5 ) / CAST( sampleRate AS REAL) * 1000.0 \
+                       END AS start, \
+                       CASE type WHEN 'EVENT' THEN \
+                         0.0
+                       ELSE \
+                        (CAST (sampleEnd AS REAL) + 1.5 ) / CAST( sampleRate AS REAL) * 1000.0 \
+                       END AS end, \
+                       session || ':' || bundle AS utts, \
+                       db_uuid,session,bundle,startItemID,endItemID,type,sampleStart,sampleEnd,sampleRate \
+                      FROM segListData")
+  
+  # set emusegs type attribute, default 'segment'
+  slType='segment'
+  if(nrow(seglist)>0){
+    # set to event only if all rows are of type EVENT
+    dTypes=sqldf("SELECT DISTINCT type FROM seglist")
+    if(nrow(dTypes)==1){
+      if(dTypes[1,1]=='EVENT'){
+        slType='event'
+      }
+    }
+  }
+  segmentList=make.emuRsegs(dbName = dbConfig[['name']],seglist = seglist,query = result[['queryStr']],type = slType)
+  return(segmentList)
+}
+
+
 equal.emusegs<-function(seglist1,seglist2,compareAttributes=TRUE,tolerance=0.0,uttsPrefix2=''){
   if(!inherits(seglist1,"emusegs")){
     stop("seglist1 is not of class emusegs")
