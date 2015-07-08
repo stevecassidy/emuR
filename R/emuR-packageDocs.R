59de68d1ef8c888cad9efa4af2fbb7767c635919
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-07-08 13:42:42 +0200

    rewrite of doc for package

--- R/emuR-packageDocs.R
+++ R/emuR-packageDocs.R
@@ -1,96 +1,77 @@
-##' EMU/R - R Programming Interface to the EMU Speech Database System
+##' emuR - Main package of the EMU Speech Database Management System
 ##' 
-##' The EMU Speech Database System for Creation, Analysis and Query of Speech
-##' Databases including acoustic and articulaty data as well as the
-##' annotations. It provides annotation, signal processing and scripting
-##' facilities, an interface to Praat, WaveSurfer as well as to Articulate
-##' Instruments. Query of annotations in EQL or SQL via data export provided.
-##' Query of signals using R. Developer: Tina John, Lasse Bombien, Steve
-##' Cassidy, Jonathan Harrington.
+##' The EMU Speech Database Management System is for creation, analysis and querying of speech
+##' databases including acoustic and articulaty data as well as the
+##' annotations themselves. It provides annotation, signal processing, evaluation and visualization
+##' facilities. The querying of annotations can be perfomed using either 
+##' EMU's own EQL2 (EMU Query Language Version 2) or SQL (intended for expert users only).
 ##' 
-##' This package is part of the EMU System and provides an interface to the R
-##' Programming Environment for the query and analysis of the speech data
-##' stored in the EMU Speech Database System and data exports from this source.
+##' This package is part of the EMU System and provides native R
+##' functions for querying and the analysis of the speech data
+##' stored in the emuDB format (see \code{vignette('emuDB_intro')}).
 ##' 
-##' \tabular{ll}{ Package: \tab emu\cr Type: \tab Package\cr Version: \tab
-##' 4.3\cr Date: \tab 2012-03-10\cr License: \tab Copyright 2012 IPS LMU
-##' Munich. All rights reserved.\cr } Workflow: \tabular{ll}{ Query database -
-##' \code{\link{emu.query}} \cr Extract labels - \code{\link{label}} \cr Get
-##' signals over segment interval - \code{\link{get_trackdata}} \cr Extract point
-##' in signal - \code{\link{dcut}} \cr Plot data - \code{\link{plot}},
-##' \code{\link{dplot}}, \code{\link{eplot}} \cr Further analysis and
-##' statistical processing \cr }
+##' Typical workflow (emuDB required):
 ##' 
-##' @name emu-package
-##' @aliases emu emu-package
-##' @docType package
-##' @author Tina John (help text)
+##' \enumerate{
+##' \item Query database - \code{\link{query}} (sometimes followed by \code{\link{requery_hier()}} or \code{\link{requery_seq()}})
+##' \item Get trackdata (e.g. formant values) for the result of a query - \code{\link{get_trackdata}}
+##' \item Data preparation - \code{\link{dcut}}, \code{\link{mel}}
+##' \item Visual data inspection - \code{\link{plot}}, \code{\link{dplot}}, \code{\link{eplot}}
+##' \item Further analysis and statistical processing
+##' }
+##' 
+##' @name emuR-package
+##' @aliases emuR emuR-package
 ##' 
 ##' Maintainer: <jmh@@phonetik.uni-muenchen.de>
 ##' @references Harrington, J. (2010). The Phonetic Analysis of Speech Corpora.
 ##' Blackwell.
 ##' @keywords package
 ##' @import methods
+##' @docType package
 ##' @examples
+##' \dontrun{
+##' # create demo data including an emuDB called "ae" 
+##' create_emuRdemoData()
 ##' 
-##'     \dontrun{
-##'     # emu.query
-##'     # Available Databases:
-##'     dbinfo()
+##' # construct path to demo emuDB
+##' path2ae = file.path(tempdir(), "emuR_demoData", "ae")
 ##' 
-##'     if(!any(dbinfo()=="demo")) {
-##'         message("No database demo found in the local EMU System - function can not be run")
-##'     } else {
-##'         # emu.query
-##'         # An EMU query    
-##'         seg = emu.query("demo","*","Phonetic = @@: | e: | ei | A | E | @@u")
-##'         seg
-##'         
-##'         #Extract Labels from segment list
-##'         seg.lab = label(seg)
-##'         seg.lab
-##'        
-##'         # emu.track
-##'         # Extraction of the tracks for the segment list
-##'         seg.sample = emu.track(seg,"samples")
-##'     
-##'         # Plot of signals
-##'         plot(seg.sample,label=seg.lab, type="l", main="waveforms")
-##'         
-##'         # dplot, eplot
-##'         # Extract track values at point in time
-##'         seg.fm = emu.track(seg,"fm")
-##'         seg.fm.5 = dcut(seg.fm, .5, prop =TRUE)
-##'         
-##'         # Plot the data as time signal and formant card
-##'         dplot(seg.fm[,1:2], seg.lab, normalise=TRUE, main = "Formants over vowel duration")
-##'         eplot(seg.fm.5[,1:2], seg.lab, dopoints=TRUE, 
-##'               doellipse=FALSE, main = "F1/F2 of vowel midpoint", form=TRUE, 
-##'               xlab = "F2 in Hz", ylab = "F1 in Hz")
-##'         
-##'         # emu.requery
-##'         # An EMU query and ...
-##'         segH = emu.query("demo","*","Phonetic = H")
-##'         segH
-##'         
-##'         # ... and requery
-##'         segHseql1 = emu.requery(segH,"Phonetic","Phonetic",sequence=-1)
-##'         segHseql1
-##'         segH.lab = label(segHseql1)
-##'     
-##'         # plot.spectral
-##'         # Plot of spectral data from 50% of aspiration duration 
-##'         segH.dft = emu.track(segH,"dft")
-##'         segH.dft.5 = dcut(segH.dft, .5,prop=TRUE)
-##'         plot(segH.dft.5,segH.lab, main = "Spectral data of aspiration")
-##'     }
+##' # load emuDB into current R session
+##' dbName = load_emuDB(path2ae)
+##' 
+##' # query loaded emuDB
+##' lvowels = query(dbName, "Phonetic = i: | u: | o:")
+##' 
+##' # extract labels from query result 
+##' lvowels.labs = label(lvowels)
+##' 
+##' # list all ssffTrackDefinitions of emuDB
+##' list_ssffTrackDefinitions(dbName)
+##' 
+##' # get formant trackdata defined in ssffTrackDefinitions "fm" for query result
+##' lvowels.fm = get_trackdata(dbName, lvowels, "fm")
+##' 
+##' # extract track values at temporal midpoint of segments
+##' lvowels.fmCut = dcut(lvowels.fm, .5, prop =TRUE)
+##' 
+##' # Plot the data as time signal and formant card
+##' dplot(lvowels.fm[,1:2], lvowels.labs, normalise=TRUE, main = "Formants over vowel duration")
+##' eplot(lvowels.fmCut[,1:2], lvowels.labs, dopoints=TRUE, 
+##'       doellipse=FALSE, main = "F1/F2 of vowel midpoint", form=TRUE, 
+##'       xlab = "F2 in Hz", ylab = "F1 in Hz")
+##'       
+##'       
+##' # Plot of spectral data from 50% of aspiration duration
+##' hs = query(dbName,"Phonetic = H")
+##' hs.labs = label(hs)
+##' hs.dft = get_trackdata(dbName, hs, "dft")
+##' hs.dftCut = dcut(hs.dft, .5, prop=TRUE)
+##' plot(hs.dftCut, hs.labs, main = "Spectral data of aspiration")
+##' 
 ##' }
 ##' 
-##'     \dontrun{emu.testsuite()}
-##' 
 NULL
-
-
 
 
 
