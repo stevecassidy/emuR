6969205b172e79e72703f409346d13d2ed9fc6c4
raphywink <raphael@phonetik.uni-muenchen.de>
2015-11-09 12:34:07 +0100

    fixed bad roxygen2 doc

--- R/emusegs.R
+++ R/emusegs.R
@@ -13,16 +13,16 @@
 ##' @examples
 ##' 
 ##' ## create a segment list file and write it out
-##' seglist.txt <- "database:demo"\
-##' query:Phonetic=vowel\
-##' type:segment\
+##' # seglist.txt <- "database:demo"\
+##' # query:Phonetic=vowel\
+##' # type:segment\
 ##' #\
-##' @@:	3059.65	3343.65	msdjc001\
-##' e:	5958.55	6244.55	msdjc002\
-##' @@u	8984.75	9288.75	msdjc003\
-##' A	11880.8	12184.8	msdjc004\
-##' E	17188.3	17366.4	msdjc005\
-##' ei	20315.2	20655.2	msdjc006"
+##' # @@:	3059.65	3343.65	msdjc001\
+##' # e:	5958.55	6244.55	msdjc002\
+##' # @@u	8984.75	9288.75	msdjc003\
+##' # A	11880.8	12184.8	msdjc004\
+##' # E	17188.3	17366.4	msdjc005\
+##' # ei	20315.2	20655.2	msdjc006"
 ##' 
 ##' \dontrun{cat(seglist.txt, file="seglist.txt")}
 ##' 
