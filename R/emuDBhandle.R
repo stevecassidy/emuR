741ca32ea744304f2f0629b164d4c54d2e0b4077
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2016-01-12 18:17:40 +0100

    summary now working

--- R/emuDBhandle.R
+++ R/emuDBhandle.R
@@ -1,18 +1,18 @@
 
 emuDBhandle = function(dbName, basePath, UUID, connectionPath){
-
+  
   con <- dbConnect(RSQLite::SQLite(), connectionPath)
   
-  if(connectionPath == ":memory:" || file.exists(file.path(basePath, paste0(dbName, database.cache.suffix)))){
-    .initialize.DBI.database(con)
-  }
-
   handle = list(dbName = dbName,
                 basePath = basePath,
                 UUID = UUID,
                 connection = con)
   
   class(handle) = "emuDBhandle"
+  
+  if(connectionPath == ":memory:" || file.exists(file.path(basePath, paste0(dbName, database.cache.suffix)))){
+    initialize_emuDbDBI(handle)
+  }
   
   return(handle)
 }
@@ -30,34 +30,45 @@ print.emuDBhandle = function(x, ...){
 ##' @export
 summary.emuDBhandle = function(object, ...){
   
-  uuid=get_UUID(dbName,dbUUID)
-  object=.load.emuDB.DBI(uuid)
-  cat("Name:\t",object[['name']],"\n")
-  cat("UUID:\t",object[['DBconfig']][['UUID']],"\n")
-  cat("Directory:\t",object[['basePath']],"\n")
-  sesss=.load.sessions.DBI(dbUUID = uuid)
-  cat("Session count:",nrow(sesss),"\n")
+  cat("Name:\t", object$dbName, "\n")
+  cat("UUID:\t", object$UUID, "\n")
+  cat("Directory:\t", object$basePath, "\n")
+  sess = list_sessions(object)
+  cat("Session count:", nrow(sess), "\n")
+  bndls = list_bundles(object)
+  cat("Bundle count:", nrow(bndls), "\n")
   
-  bndlCnt=.get.bundle.count.DBI(uuid)
-  
-  cat("Bundle count:",bndlCnt,"\n")
-  itCntQ=paste0("SELECT count(*) FROM items WHERE db_uuid='",uuid,"'")
-  itCntDf=dbGetQuery(get_emuDBcon(uuid),itCntQ)
-  itemCnt=itCntDf[[1]]
-  liCntQ=paste0("SELECT count(*) FROM links WHERE db_uuid='",uuid,"'")
-  liCntDf=dbGetQuery(get_emuDBcon(uuid),liCntQ)
-  linkCnt=liCntDf[[1]]
-  cat("Annotation item count: ",itemCnt,", links count: ",linkCnt,"\n")
+  itCntQ = paste0("SELECT count(*) FROM items WHERE db_uuid='", object$UUID, "'")
+  itCntDf = dbGetQuery(object$connection, itCntQ)
+  itemCnt = itCntDf[[1]]
+  liCntQ = paste0("SELECT count(*) FROM links WHERE db_uuid='", object$UUID, "'")
+  liCntDf = dbGetQuery(object$connection, liCntQ)
+  linkCnt = liCntDf[[1]]
+  cat("Annotation item count: ", itemCnt, ", links count: ", linkCnt, "\n")
   cat("\nDatabase configuration:\n\n")
-  summary(object[['DBconfig']])
-  #cat("SSFF track definitions:\n")
-  # TODO 
   
+  dbConfig = get_DBconfig(object)
+  cat("SSFF track definitions:\n")
+  ssffTrackDefs = list_ssffTrackDefinitions(object)
+  print(ssffTrackDefs)
+  cat("\n")
+  cat("Level definitions:\n")
+  levelDefs = list_levelDefinitions(object)
+  print(levelDefs)
+  lblGrps = list_labelGroups(object)
+  if(nrow(lblGrps) > 0){
+    cat("Database label group definitions:\n")
+    print(lblGrps)
+  }
+  cat("\n")
+  cat("Link definitions:\n")
+  linkDefs = list_linkDefinitions(object)
+  print(linkDefs)
 }
 
 ##########################
 # FOR DEVELOPMENT
-#handle = emuDBhandle(dbName = "test12", basePath = "/you/smell/like/poo", UUID = "3412D5E3-E0EA-4E81-9F1C-E0A864D0D403", ":memory:")
+# handle = emuDBhandle(dbName = "test12", basePath = "/you/smell/like/poo", UUID = "3412D5E3-E0EA-4E81-9F1C-E0A864D0D403", ":memory:")
 # ae1 = load_emuDB("~/Desktop/emuR_demoData/ae", inMemoryCache = T)
 # ae2 = load_emuDB("~/Desktop/emuR_demoData/ae", inMemoryCache = F)
 # print(summary(ae1))
