c822dddfc6566f58ee7ff316a1a922d2400d8cd5
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2016-01-19 18:55:22 +0100

    using SE version of filter

--- R/emuR-database.annotJSON.R
+++ R/emuR-database.annotJSON.R
@@ -62,7 +62,7 @@ bundleAnnotDFsToAnnotJSONchar <- function(emuDBhandle, annotDFs){
     levels[[length(levels) + 1]] = list(
       items = apply(levelItems, 1, function(r) {
       
-      labels = apply(filter(annotDFs$labels, itemID == as.numeric(r[1])), 1, function(r2) list(name = r2[3], value = r2[4]))
+      labels = apply(filter_(annotDFs$labels, ~(itemID == as.numeric(r[1]))), 1, function(r2) list(name = r2[3], value = r2[4]))
       res = NULL
       if(r[3] == "ITEM"){
         res = list(id = as.numeric(r[1]),
