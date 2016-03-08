f4e2a4afce508dcfa6dc2c3ffb2ea1c839e0471f
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2016-03-08 11:02:47 +0100

    removed demos

--- demo/demo.vowels.fm.R
+++
@@ -1,6 +0,0 @@
-packpath = .path.package(package = "emuR", quiet = FALSE)
-  sepa = .Platform$file.sep
-  nfile = "demo-vowel-fm.dat"
-  filepath = paste(packpath,sepa,"data",sepa,nfile,sep = "")
- 
- demo.vowels.fm <- read.trackdata(filepath)
