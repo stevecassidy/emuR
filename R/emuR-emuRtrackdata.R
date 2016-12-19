05608c8cf39bda40778c14af76311adb3be627a6
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2016-12-19 16:42:29 +0100

    and back again...

--- R/emuR-emuRtrackdata.R
+++ R/emuR-emuRtrackdata.R
@@ -1,15 +1,12 @@
-requireNamespace("data.table", quietly = T)
-requireNamespace("dplyr", quietly = T)
-
 ##' create emuRtrackdata object
 ##' 
 ##' Joins \code{\link{emuRsegs}} and \code{\link{trackdata}} objects
 ##' to create an \code{\link{emuRtrackdata}} object that is a sub-class of
-##' a \code{data.table} (and \code{\link{data.frame}}) object. This object 
+##' a \code{\link[data.table]{data.table}} (and \code{\link{data.frame}}) object. This object 
 ##' can be viewed as a flat version of a \code{\link{trackdata}} object that also 
 ##' contains all the information of a \code{\link{emuRsegs}} object. It is meant to
 ##' ease integration with other packages as it is based on the well known 
-##' \code{data.table} and \code{\link{data.frame}} objects.
+##' \code{\link[data.table]{data.table}} and \code{\link{data.frame}} objects.
 ##' @param sl seglist of class \code{\link{emuRsegs}}
 ##' @param td \code{\link{trackdata}} object generated from sl
 ##' @return emuRtrackdata object
