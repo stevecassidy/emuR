70252a242adf8228b6408d9fd7d69dd400226be7
Nina <nina.poerner@gmail.com>
2017-01-26 15:05:55 +0100

    Implemented convert_PlainTextCollection

--- R/emuR-import_transcriptions.R
+++ R/emuR-import_transcriptions.R
@@ -1,10 +1,111 @@
-import_transcription_files <- function(handle,
-                                       source,
-                                       txtExt = 'txt',
-                                       transcriptionLevel = "Transcription",
-                                       transcriptionLabel = "Transcription",
-                                       cleanWhitespaces = TRUE,
-                                       verbose = TRUE)
+##' 
+##' 
+##' @export
+##' @seealso
+
+convert_PlainTextCollection <- function(dbName,
+                                        sourceDirTxt,
+                                        sourceDirMedia,
+                                        targetDir,
+                                        txtExtension = 'txt',
+                                        mediaFileExtension = 'wav',
+                                        transcriptionLevel = "Transcription",
+                                        transcriptionLabel = "Transcription",
+                                        cleanWhitespaces = TRUE,
+                                        verbose = TRUE)
+{
+  file_pair_list = create_filePairList(sourceDirMedia, sourceDirTxt, mediaFileExtension, txtExtension)
+  
+  if(nrow(file_pair_list) == 0)
+  {
+    stop("Could not find any file pairs in ", sourceDir)
+  }
+  
+  all_sessions = list()
+  for(idx in 1:nrow(file_pair_list))
+  {
+    mediaFile = file_pair_list[idx, 1]
+    txtFile = file_pair_list[idx, 2]
+    session = get_bpfSession(mediaFile, sourceDirMedia)
+    
+    mediaDir = normalizePath(dirname(mediaFile), winslash = "/")
+    txtDir = normalizePath(dirname(txtFile), winslash = "/")
+    
+    all_sessions[[session]] = c(mediaDir, txtDir)
+  }
+  
+  create_emuDB(
+    name = dbName,
+    targetDir = targetDir,
+    mediaFileExtension = mediaFileExtension,
+    store = TRUE
+  )
+  handle = load_emuDB(file.path(targetDir, paste0(dbName, emuDB.suffix)))
+  
+  if (verbose)
+  {
+    progress = 0
+    cat("INFO: Importing media and text files for", length(names(all_sessions)), "sessions ...\n")
+    pb = utils::txtProgressBar(
+      min = 0,
+      max = length(names(all_sessions)),
+      initial = progress,
+      style = 3
+    )
+    utils::setTxtProgressBar(pb, progress)
+  }
+  
+  for(session in names(all_sessions))
+  {
+    import_mediaFiles(handle, all_sessions[[session]][1], targetSessionName = session, verbose = FALSE)
+    
+    import_transcriptionFiles(
+      handle,
+      source = all_sessions[[session]][2],
+      txtExt = txtExtension,
+      transcriptionLevel = transcriptionLevel,
+      transcriptionLabel = transcriptionLabel,
+      targetSessionName = session,
+      verbose = FALSE,
+      cleanWhitespaces = cleanWhitespaces,
+      rewriteAllAnnots = FALSE,
+      changeDBConfig = FALSE
+    )
+    
+    if (verbose)
+    {
+      progress = progress + 1
+      utils::setTxtProgressBar(pb, progress)
+    }
+  }
+  
+  if(verbose)
+  {
+    cat("\n")
+  }
+  
+  add_levelDefinition(handle, transcriptionLevel, "ITEM", verbose = verbose)
+  if (transcriptionLabel != transcriptionLevel) {
+    add_attributeDefinition(handle, transcriptionLevel, transcriptionLabel, verbose = verbose)
+  }
+  
+  rewrite_allAnnots(handle, verbose = verbose)
+  
+  return(handle)
+}
+
+
+
+import_transcriptionFiles <- function(handle,
+                                      source,
+                                      txtExt = 'txt',
+                                      transcriptionLevel = "Transcription",
+                                      transcriptionLabel = "Transcription",
+                                      cleanWhitespaces = TRUE,
+                                      rewriteAllAnnots = TRUE,
+                                      changeDBConfig = TRUE,
+                                      targetSessionName = "0000",
+                                      verbose = TRUE)
 {
   source = suppressWarnings(normalizePath(source))
   
@@ -28,7 +129,7 @@ import_transcription_files <- function(handle,
       progress = 0
       cat("INFO: Importing ",
           length(txtList),
-          " transcriptions...\n")
+          " transcriptions into session ", targetSessionName, " ...\n")
       pb = utils::txtProgressBar(
         min = 0,
         max = length(txtList),
@@ -38,7 +139,7 @@ import_transcription_files <- function(handle,
       utils::setTxtProgressBar(pb, progress)
     }
     
-    if (length(txtList) != nrow(list_bundles(handle)))
+    if (length(txtList) != nrow(list_bundles(handle, targetSessionName)))
     {
       stop()
     }
@@ -49,22 +150,9 @@ import_transcription_files <- function(handle,
     {
       path = file.path(source, txtfile)
       bundle = sub('[.][^.]*$', '', txtfile)
+      session = targetSessionName
       
-      queryTxt = paste0("SELECT session, name, sample_rate FROM bundle WHERE name=='", bundle, "'")
-      res = DBI::dbGetQuery(handle$connection, queryTxt)
-      if (nrow(res) == 0)
-      {
-        stop()
-      }
-      
-      if(nrow(res) > 1)
-      {
-        stop()
-      }
-      
-      session = res[1, "session"]
-      bundle = res[1, "name"]
-      samplerate = res[1, "sample_rate"]
+      samplerate = bas_get_samplerate(handle, session, bundle)
       
       transcription = paste(readLines(path), collapse = " ")
       transcription = stringr::str_trim(transcription)
@@ -73,7 +161,7 @@ import_transcription_files <- function(handle,
       if (cleanWhitespaces)
       {
         transcription = stringr::str_replace_all(transcription, "\\s+", " ")
-    }
+      }
       
       item_id = bas_get_max_id(handle, session, bundle) + 1
       queryTxt = paste0(
@@ -123,8 +211,16 @@ import_transcription_files <- function(handle,
   }
   bas_clear(handle)
   
-  add_levelDefinition(handle, transcriptionLevel, "ITEM")
-  if (transcriptionLabel != transcriptionLevel) {
-    add_attributeDefinition(handle, transcriptionLevel, transcriptionLabel)
+  if(changeDBConfig)
+  {
+    add_levelDefinition(handle, transcriptionLevel, "ITEM", verbose = verbose)
+    if (transcriptionLabel != transcriptionLevel) {
+      add_attributeDefinition(handle, transcriptionLevel, transcriptionLabel, verbose = verbose)
+    }
+  }
+  
+  if(rewriteAllAnnots)
+  {
+    rewrite_allAnnots(handle, verbose = verbose)
   }
 }
