ff7424ee902f47f3482047fbe1b0f9b4f78d5205
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2019-09-10 18:05:44 +0200

    only checking non NA values to set slType (fixes #216)

--- R/emuR-create_seglists.R
+++ R/emuR-create_seglists.R
@@ -477,7 +477,7 @@ convert_queryResultToEmuRsegs <- function(emuDBhandle,
   slType='segment'
   if(nrow(seglist)>0){
     # set to event only if all rows are of type EVENT
-    if(all(seglist$type == "EVENT")){
+    if(all(seglist$type[!is.na(seglist$type)] == "EVENT")){
       slType='event'
     }
   }
